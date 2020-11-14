83db73e04ed4b28197403bc21dc7544d900b3c78
cxy <37500268+chenxygh@users.noreply.github.com>
2020-11-14 10:58:13 +0800

    to sublime for a moment

--- code/main.c
+++ code/main.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define DATASIZE 21
 #define SAVINGLEN 190
+#define MAXWEIGHT 20
+#define MAXDISTANCE 50
 
 typedef struct {
-    int name;
+    int l;
+    int r;
     double value;
 } Saving;
 
+typedef struct {
+    int route[43];
+    int left;
+    int right;
+} Routes;
+
 double distance[DATASIZE][DATASIZE];
 Saving saving_distance[SAVINGLEN];
+Routes routes[5];
+char citySelected[21];
 
 void read () {
     FILE *fp = NULL;
@@ -30,8 +42,8 @@ void createSD () {
     int length = 0;
     for (int i = 1; i < DATASIZE; ++i) {
         for (int j = i + 1; j < DATASIZE; ++j) {
-            saving_distance[length].name = i;
-            saving_distance[length].name = saving_distance[length].name * 100 + j;
+            saving_distance[length].l = i;
+            saving_distance[length].r = j;
             saving_distance[length++].value = distance[0][i] + distance[0][j] - distance[i][j];
         }
     } 
@@ -55,17 +67,30 @@ void quickSort (Saving *elem, int left, int right) {
     quickSort (elem, left, pivotPos - 1);
     quickSort (elem, pivotPos + 1, right);
 }
+
+void createRoutes () {
+    memset (routes, 0, sizeof (routes));
+    memset (citySelected, 0, sizeof (citySelected));
+
+
+    int i = 0, j = 0, cnt = 0;
+    while (cnt >= 20) {
+
+
+    }
+}
+
 int main () {
     read ();
     createSD ();
     quickSort (saving_distance, 0, SAVINGLEN);
     for (int i = 0; i < SAVINGLEN; ++i) {
-        printf ("%04d: %5lf\n", saving_distance[i].name, saving_distance[i].value);
+        printf ("%02d%02d: %5lf\n", saving_distance[i].l, saving_distance[i].r, saving_distance[i].value);
     }
     printf ("\n");
      
     for (int i = 0; i < SAVINGLEN; ++i) {
-        printf ("%04d: %5lf\n", saving_distance[i].name, saving_distance[i].value);
+        printf ("%02d%02d: %5lf\n", saving_distance[i].l, saving_distance[i].r, saving_distance[i].value);
     }
     return 0;
 }
