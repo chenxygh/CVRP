afac6706df8953823065903236eb32c0321f0805
cxy <37500268+chenxygh@users.noreply.github.com>
2020-11-15 11:04:18 +0800

    translate tab to expandtab

--- code/main.c
+++ code/main.c
@@ -51,7 +51,7 @@ void createSD () {
             saving_distance[length].used = 0;
             saving_distance[length++].value = distance[0][i] + distance[0][j] - distance[i][j];
         }
-    } 
+    }
 }
 
 int partition (Saving *elem, int left, int right) {
@@ -77,111 +77,111 @@ void createRoutes () {
     memset (routes, 0, sizeof (routes));
     memset (citySelected, 0, sizeof (citySelected));
     for (int i = 0; i < 5; ++i) {
-    	routes[i].left = 21;
-    	routes[i].right = 22;
+        routes[i].left = 21;
+        routes[i].right = 22;
     }
 
     int cnt = 0;
     double length = 0, weight = 0;
     while (cnt <= 20) {
-    	for (int i = 0; i < SAVINGLEN; ++i) {
-    		// 假设不会出现只送一个城市就超出里程或者载重量的情形
-    		// case 1: 路径生成初始时刻
-    		if (routes[routesSize].route[routes[routesSize].left + 1] == routes[routesSize].route[routes[routesSize].right - 1]) {// head adjacent to tail
-    			if (cnt == 19) {
-	    			int k = 0;
-	    			while (citySelected[k] != 0) ++k;
-	    			citySelected[k] = 2;
-	    			routes[routesSize].route[routes[routesSize].right++] = k;
-					routesSize++;
-					allLen += 2 * distance[0][k];
-	    			return;
-	    		}
-    			while (i < SAVINGLEN && saving_distance[i].used == 1) ++i;
-    			// 防止数组越界，虽然这里不会，但是加上异常处理是一个好习惯
-    			if (i >= SAVINGLEN) {
-    				printf("saving overflow\n");
-    				return;
-    			}
-    			int c1 = saving_distance[i].l, c2 = saving_distance[i].r;
-    			// 这里, 所剩城市 >= 2, 所以必能跳出 while
-    			while (citySelected[c1] != 0 || citySelected[c2] != 0) {
-    				++i;
-    				while (i < SAVINGLEN && saving_distance[i].used == 1) ++i;
-    				if (i >= SAVINGLEN) {
-	    				printf("saving overflow\n");
-	    				return;
-	    			}
-    				c1 = saving_distance[i].l;
-    				c2 = saving_distance[i].r;
-    			}
-    			length = distance[0][c1] + distance[0][c2] + distance[c1][c2];
-    			weight = weights[c1] + weights[c2];
-    			routes[routesSize].route[routes[routesSize].right++] = c1;
-    			routes[routesSize].route[routes[routesSize].right++] = c2;
-    			citySelected[c1] = citySelected[c2] = 1;
-    			saving_distance[i].used = 1;
-    			cnt += 2;
-    			continue;
-    		}
+        for (int i = 0; i < SAVINGLEN; ++i) {
+            // 假设不会出现只送一个城市就超出里程或者载重量的情形
+            // case 1: 路径生成初始时刻
+            if (routes[routesSize].route[routes[routesSize].left + 1] == routes[routesSize].route[routes[routesSize].right - 1]) {// head adjacent to tail
+                if (cnt == 19) {
+                    int k = 0;
+                    while (citySelected[k] != 0) ++k;
+                    citySelected[k] = 2;
+                    routes[routesSize].route[routes[routesSize].right++] = k;
+                    routesSize++;
+                    allLen += 2 * distance[0][k];
+                    return;
+                }
+                while (i < SAVINGLEN && saving_distance[i].used == 1) ++i;
+                // 防止数组越界，虽然这里不会，但是加上异常处理是一个好习惯
+                if (i >= SAVINGLEN) {
+                    printf("saving overflow\n");
+                    return;
+                }
+                int c1 = saving_distance[i].l, c2 = saving_distance[i].r;
+                // 这里, 所剩城市 >= 2, 所以必能跳出 while
+                while (citySelected[c1] != 0 || citySelected[c2] != 0) {
+                    ++i;
+                    while (i < SAVINGLEN && saving_distance[i].used == 1) ++i;
+                    if (i >= SAVINGLEN) {
+                        printf("saving overflow\n");
+                        return;
+                    }
+                    c1 = saving_distance[i].l;
+                    c2 = saving_distance[i].r;
+                }
+                length = distance[0][c1] + distance[0][c2] + distance[c1][c2];
+                weight = weights[c1] + weights[c2];
+                routes[routesSize].route[routes[routesSize].right++] = c1;
+                routes[routesSize].route[routes[routesSize].right++] = c2;
+                citySelected[c1] = citySelected[c2] = 1;
+                saving_distance[i].used = 1;
+                cnt += 2;
+                continue;
+            }
 
 
-    		// case 2: 路径中已有城市
-    		while (i < SAVINGLEN && saving_distance[i].used == 1) ++i;
-			if (i >= SAVINGLEN) {
-				for (int i = routes[routesSize].left + 1, j = routes[routesSize].right; i < j; ++i) {
-					citySelected[routes[routesSize].route[i]] = 2;
-				}
-				routesSize++;
-				allLen += length;
-				break;
-			}
-			int c1 = saving_distance[i].l, c2 = saving_distance[i].r;
-			// 找路径的首尾城市相关的 saving
-			while (!((citySelected[c1] == 0 && citySelected[c2] == 1) || (citySelected[c1] == 1 && citySelected[c2] == 0)) && ++i < SAVINGLEN) {
-				while (i < SAVINGLEN && saving_distance[i].used == 1) ++i;
-				if (i >= SAVINGLEN) break;
-				c1 = saving_distance[i].l;
-				c2 = saving_distance[i].r;
-			}
-			// 已无法合并
-			if (i >= SAVINGLEN) {
-				for (int i = routes[routesSize].left + 1, j = routes[routesSize].right; i < j; ++i) {
-					citySelected[routes[routesSize].route[i]] = 2;
-				}
-				routesSize++;
-				allLen += length;
-				break;
-			}
-			
-			int city_in = citySelected[c1]? c1: c2;
-			int head = routes[routesSize].left + 1, tail = routes[routesSize].right - 1;
-			if (city_in != routes[routesSize].route[head] && city_in != routes[routesSize].route[tail]) continue;
+            // case 2: 路径中已有城市
+            while (i < SAVINGLEN && saving_distance[i].used == 1) ++i;
+            if (i >= SAVINGLEN) {
+                for (int i = routes[routesSize].left + 1, j = routes[routesSize].right; i < j; ++i) {
+                    citySelected[routes[routesSize].route[i]] = 2;
+                }
+                routesSize++;
+                allLen += length;
+                break;
+            }
+            int c1 = saving_distance[i].l, c2 = saving_distance[i].r;
+            // 找路径的首尾城市相关的 saving
+            while (!((citySelected[c1] == 0 && citySelected[c2] == 1) || (citySelected[c1] == 1 && citySelected[c2] == 0)) && ++i < SAVINGLEN) {
+                while (i < SAVINGLEN && saving_distance[i].used == 1) ++i;
+                if (i >= SAVINGLEN) break;
+                c1 = saving_distance[i].l;
+                c2 = saving_distance[i].r;
+            }
+            // 已无法合并
+            if (i >= SAVINGLEN) {
+                for (int i = routes[routesSize].left + 1, j = routes[routesSize].right; i < j; ++i) {
+                    citySelected[routes[routesSize].route[i]] = 2;
+                }
+                routesSize++;
+                allLen += length;
+                break;
+            }
 
-			// 考虑 weight 和 length 约束
-			int city_out = citySelected[c1]? c2: c1;
-			double temp_len = length + distance[city_in][city_out] + distance[0][city_out] - distance[0][city_in];
-			double temp_weight = weight + weights[city_out];
-			if (temp_len > MAXDISTANCE || temp_weight > MAXWEIGHT) {
-				for (int i = routes[routesSize].left + 1, j = routes[routesSize].right; i < j; ++i) {
-					citySelected[routes[routesSize].route[i]] = 2;
-				}
-				routesSize++;
-				allLen += length;
-				break;
-			}
-			length = temp_len;
-			weight = temp_weight;
-			citySelected[city_out] = 1;
-			saving_distance[i].used = 1;
-			cnt++;
-			if (city_in == routes[routesSize].route[head]) {
-				routes[routesSize].route[routes[routesSize].left--] = city_out;
-			}
-			if (city_in == routes[routesSize].route[tail]) {
-    			routes[routesSize].route[routes[routesSize].right++] = city_out;
-			}
-    	}
+            int city_in = citySelected[c1]? c1: c2;
+            int head = routes[routesSize].left + 1, tail = routes[routesSize].right - 1;
+            if (city_in != routes[routesSize].route[head] && city_in != routes[routesSize].route[tail]) continue;
+
+            // 考虑 weight 和 length 约束
+            int city_out = citySelected[c1]? c2: c1;
+            double temp_len = length + distance[city_in][city_out] + distance[0][city_out] - distance[0][city_in];
+            double temp_weight = weight + weights[city_out];
+            if (temp_len > MAXDISTANCE || temp_weight > MAXWEIGHT) {
+                for (int i = routes[routesSize].left + 1, j = routes[routesSize].right; i < j; ++i) {
+                    citySelected[routes[routesSize].route[i]] = 2;
+                }
+                routesSize++;
+                allLen += length;
+                break;
+            }
+            length = temp_len;
+            weight = temp_weight;
+            citySelected[city_out] = 1;
+            saving_distance[i].used = 1;
+            cnt++;
+            if (city_in == routes[routesSize].route[head]) {
+                routes[routesSize].route[routes[routesSize].left--] = city_out;
+            }
+            if (city_in == routes[routesSize].route[tail]) {
+                routes[routesSize].route[routes[routesSize].right++] = city_out;
+            }
+        }
     }
 }
 
@@ -193,7 +193,7 @@ int main () {
         printf ("%02d%02d: %5lf\n", saving_distance[i].l, saving_distance[i].r, saving_distance[i].value);
     }
     printf ("\n");
-     
+
     for (int i = 0; i < SAVINGLEN; ++i) {
         printf ("%02d%02d: %5lf\n", saving_distance[i].l, saving_distance[i].r, saving_distance[i].value);
     }
@@ -202,10 +202,10 @@ int main () {
     createRoutes ();
     printf("routesSize: %d, allLen: %lf\n", routesSize, allLen);
     for (int i = 0; i < routesSize; ++i) {
-    	for (int j = routes[i].left, k = routes[i].right; j <= k; ++j) {
-    		printf("%d ", routes[i].route[j]);
-    	}
-    	printf("\n");
+        for (int j = routes[i].left, k = routes[i].right; j <= k; ++j) {
+            printf("%d ", routes[i].route[j]);
+        }
+        printf("\n");
     }
 
     return 0;
